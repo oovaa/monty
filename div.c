40e4f593a1d71d6d6f28991637cb80ca953c2abf
oovaa <omarvx211@gmail.com>
2023-11-16 06:16:59 +0200

    fix div at 16-06

--- div.c
+++ div.c
@@ -10,15 +10,16 @@ void f_div(stack_t **head, unsigned int line_number)
 		freeAll_and_exit(*head);
 	}
 
-	val = (*head)->n;
-	if (val == 0)
+	if ((*head)->n == 0)
 	{
-		fprintf(stderr, "L%d: division by zer\n", line_number);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		freeAll_and_exit(*head);
 	}
-	
+
+	val = (*head)->next->n / (*head)->n;
+
 	pop(head, line_number);
 
-	(*head)->n /= val;
+	(*head)->n = val;
 
 }
