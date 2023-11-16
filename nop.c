dd8c23d3242138512cac055d86a4f3a8d1ab2159
Reem Osama <ireosama1@gmail.com>
2023-11-15 23:13:22 -0800

    adding betty documentation to nop

--- nop.c
+++ nop.c
@@ -1,5 +1,13 @@
 #include "monty.h"
 
+/**
+ * nop - The opcode nop doesn’t do anything.
+ * @head: a pointer to the head of the list
+ * @line_number: the number of the line to be printed if there's an error
+ *
+ * Return: Nothing.
+ */
+
 void nop(stack_t **head, unsigned int line_number)
 {
 	(void) line_number;
